a1a3bea3e2610ef68c21c5bd741d74b13dfc002d
Brandon <wagnerbm@amazon.com>
2022-05-19 11:32:02 +0200

    pricing controller fix

--- pricing-controller.sh
+++ pricing-controller.sh
@@ -2,15 +2,15 @@
 
 IFS=$'\n'
 while true; do 
-    for line in $(kubectl get nodes --no-headers --selector node.kubernetes.io/instance-type --selector karpenter.sh/capacity-type -L node.kubernetes.io/instance-type -L karpenter.sh/capacity-type -L topology.kubernetes.io/zone | tr -s " "); do
+    for line in $(kubectl get nodes --no-headers --selector node.kubernetes.io/instance-type --selector karpenter.sh/capacity-type -L node.kubernetes.io/instance-type -L karpenter.sh/capacity-type -L topology.kubernetes.io/zone -L node.k8s.aws/spot-percent-discount | tr -s " "); do
         node=$(echo ${line} | cut -d' ' -f1)
         instanceType=$(echo ${line} | cut -d' ' -f6)
         capacityType=$(echo ${line} | cut -d' ' -f7)
         zone=$(echo ${line} | cut -d' ' -f8)
-        if [[ ${capacityType} = "spot" ]]; then 
+        spotDiscount=$(echo ${line} | cut -d' ' -f9)
+        if [[ ${capacityType} = "spot" ]] && [[ -z ${spotDiscount} ]]; then 
             spotPrice=$(aws ec2 describe-spot-price-history --region "${zone%?}" --availability-zone "${zone}" --instance-types "${instanceType}" --max-items=1 --product-descriptions='Linux/UNIX' | jq -r '.SpotPriceHistory[] .SpotPrice')
             odPrice=$(odpricing "${instanceType}")
-            set -x
             discount=$(echo "${spotPrice} ${odPrice}" | awk '{print ($1 / $2) * 100}')
             discount_int=$(printf %.0f $discount)
             kubectl label nodes ${node} "node.k8s.aws/spot-percent-discount=${discount_int}" --overwrite
